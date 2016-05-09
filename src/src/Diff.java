2989923053d81de7292bfa65671003cf556b7fe4
kaishuoy <kaishuoy@yahoo.com>
2016-05-09 20:36:21 +1200

    initial commit

---
+++ src/src/Diff.java
@@ -0,0 +1,91 @@
+package src;
+
+/**
+ * Compares two files and finds the differences between them
+ * 
+ * Credits to Princeton for original version, minor modifications by Kai
+ * http://introcs.cs.princeton.edu/java/96optimization/Diff.java.html
+ *
+ */
+
+public class Diff {
+	static int prevAff = 3; //tracks changes in addition/deletion state
+
+	public static void diff(String[] args) {
+		prevAff = 3; 
+		//debugging only, program should never actually get here...
+		if (args.length == 0){
+			args = new String[] { "Castling.java", "Castlinga.java" };
+			System.out.println("WARNING:No arguments supplied for diff(), using debug case...");
+		}
+		// read in lines of each file
+		In in0 = new In(args[0]);
+		In in1 = new In(args[1]);
+		String[] x = in0.readAllLines();
+		String[] y = in1.readAllLines();
+
+		// number of lines of each file
+		int M = x.length;
+		int N = y.length;
+
+		// opt[i][j] = length of LCS of x[i..M] and y[j..N]
+		int[][] opt = new int[M + 1][N + 1];
+
+		// compute length of LCS and all subproblems via dynamic programming
+		for (int i = M - 1; i >= 0; i--) {
+			for (int j = N - 1; j >= 0; j--) {
+				if (x[i].equals(y[j]))
+					opt[i][j] = opt[i + 1][j + 1] + 1;
+				else
+					opt[i][j] = Math.max(opt[i + 1][j], opt[i][j + 1]);
+			}
+		}
+
+		// recover LCS itself and print out non-matching lines to standard
+		// output
+		int i = 0, j = 0;
+		while (i < M && j < N) {
+			if (x[i].equals(y[j])) {
+				i++;
+				j++;
+			} else {
+				if (opt[i + 1][j] >= opt[i][j + 1]) {
+					if (prevAff != 0) {
+						System.out.println("DELETIONS:");
+						prevAff = 0;
+					}
+					System.out.println("< " + x[i++]);
+				} else {
+					if (prevAff != 1) {
+						System.out.println("ADDITIONS:");
+						prevAff = 1;
+					}
+					System.out.println("> " + y[j++]);
+				}
+			}
+		}
+
+		// dump out one remainder of one string if the other is exhausted
+		while (i < M || j < N) {
+			if (i == M) {
+				if (prevAff != 0) {
+					System.out.println("DELETIONS:");
+					prevAff = 0;
+				}
+				System.out.println("> " + y[j++]);
+			} else if (j == N){
+				if (prevAff != 1) {
+					System.out.println("ADDITIONS:");
+					prevAff = 1;
+				}
+				System.out.println("< " + x[i++]);
+			}
+		}
+	}
+
+	//debugging purposes
+	public static void main(String[] args) {
+		diff(new String[] { "testdiff", "testdiff2" });
+	}
+
+}
---
+++ src/src/Marker.java
@@ -0,0 +1,267 @@
+package src;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * A assignment marking assistant designed to make marking student code easier
+ * Compares two jars based on required submission guidelines and compares
+ * student version with original version
+ * 
+ * Also detects additional java files submitted and prints them out
+ * 
+ * @author Kai Yang
+ *
+ */
+public class Marker {
+
+	public final String dir = "marking/"; // folder name
+	private List<String> files;
+	int addFiles = 0;
+	int missingFiles = 0;
+
+	public Marker() {
+		files = new ArrayList<String>();
+	}
+
+	/**
+	 * Unzips the master jar to working directory and adds all files to filelist
+	 * 
+	 * @param filename
+	 */
+	private void unzipMasterJar(String filename) {
+		try {
+			// Open the zip file
+			ZipFile zipFile = new ZipFile(filename);
+			Enumeration<?> enu = zipFile.entries();
+			while (enu.hasMoreElements()) {
+				ZipEntry zipEntry = (ZipEntry) enu.nextElement();
+
+				String name = zipEntry.getName();
+
+				// Do we need to create a directory ?
+				File file = new File(name);
+				if (name.endsWith("/")) {
+					file.mkdirs();
+					continue;
+				}
+
+				File parent = file.getParentFile();
+				if (parent != null) {
+					parent.mkdirs();
+				}
+
+				// Extract the file
+				InputStream is = zipFile.getInputStream(zipEntry);
+				FileOutputStream fos = new FileOutputStream(file);
+				byte[] bytes = new byte[1024];
+				int length;
+				while ((length = is.read(bytes)) >= 0) {
+					fos.write(bytes, 0, length);
+				}
+				if (getFileExtension(file).equals("java")) {
+					files.add(name);
+				}
+				is.close();
+				fos.close();
+
+			}
+			zipFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Master unzip done");
+
+	}
+
+	/**
+	 * Unzips a zip(or jar) file to a specified directory affix
+	 * 
+	 * @param filename
+	 *            zip file to be unzipped
+	 * @param affix
+	 *            folder to unzip it to
+	 */
+	public void unzipJar(String filename, String affix) {
+
+		try {
+			// Open the zip file
+			ZipFile zipFile = new ZipFile(filename);
+			Enumeration<?> enu = zipFile.entries();
+			while (enu.hasMoreElements()) {
+				ZipEntry zipEntry = (ZipEntry) enu.nextElement();
+
+				String name = affix + zipEntry.getName();
+
+				// Do we need to create a directory ?
+				File file = new File(name);
+				if (name.endsWith("/")) {
+					file.mkdirs();
+					continue;
+				}
+
+				File parent = file.getParentFile();
+				if (parent != null) {
+					parent.mkdirs();
+				}
+
+				// Extract the file
+				InputStream is = zipFile.getInputStream(zipEntry);
+				FileOutputStream fos = new FileOutputStream(file);
+				byte[] bytes = new byte[1024];
+				int length;
+				while ((length = is.read(bytes)) >= 0) {
+					fos.write(bytes, 0, length);
+				}
+				is.close();
+				fos.close();
+
+			}
+			zipFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Student unzip done");
+	}
+
+	/**
+	 * Runs diff on each file to find differences
+	 */
+	private void diffFiles() {
+		for (String s : files) {
+			System.out.println(s);
+			File studentCurrent = new File(dir+s);
+			if(studentCurrent.exists())	Diff.diff(new String[] { s, dir + s });
+			else {
+				System.out.println("ERROR: This file was not present in student submission");
+				missingFiles++;
+			}
+			
+			System.out.println("================END OF FILE================");
+			System.out.println("");
+		}
+	}
+
+	/**
+	 * Recursively goes through a directory to find all java files not present
+	 * in mark scheme
+	 * 
+	 * @param folder
+	 * @throws FileNotFoundException
+	 */
+	private void findExtras(File folder) throws FileNotFoundException {
+		File[] fileList = folder.listFiles();
+		for (File f : fileList) {
+			String filePath = f.getPath();
+			String[] filePathSplit = filePath.split("\\\\|/");
+			ArrayList<String> filePathSplitList = new ArrayList(Arrays.asList(filePathSplit));
+			filePathSplitList.remove(0);
+			filePath = String.join("/", filePathSplitList);
+
+			if (filePath.endsWith("java") && !files.contains(filePath)) {
+				System.out.println(f.getPath());
+				Scanner sc = new Scanner(f);
+				addFiles++;
+				while (sc.hasNextLine()) {
+					System.out.println(sc.nextLine());
+				}
+				System.out.println("===========END OF FILE===========");
+				System.out.println("");
+				sc.close();
+			}
+			if (f.isDirectory()) {
+				findExtras(f);
+			}
+		}
+	}
+
+	/**
+	 * Gets a file extension
+	 * 
+	 * @param file
+	 * @return
+	 */
+	private static String getFileExtension(File file) {
+		String fileName = file.getName();
+		if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+			return fileName.substring(fileName.lastIndexOf(".") + 1);
+		else
+			return "";
+	}
+
+	/**
+	 * Recursively deletes files
+	 * @param f
+	 * @throws IOException
+	 */
+	private void delete(File f) throws IOException {
+		System.out.println(f);
+		if (f.isDirectory()) {
+			for (File c : f.listFiles())
+				delete(c);
+		}
+		if(!f.delete()){
+			System.out.println("fail");
+		}
+	}
+
+	/**
+	 * Diffs two jars based on the filestructure, supply it with a file
+	 * containing target filestructure(one provided as example without
+	 * whitespace), name of master jar(dave's copy), name of student jar
+	 *
+	 * @param args
+	 * @throws FileNotFoundException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		// default debugging case
+		if (args.length == 0) {
+			System.out.println("Warning no arguments supplied, defaulting to debug arguments");
+			args = new String[] { "shapes.jar", "SWEN221A3.jar" };
+		}
+		// create marker
+		Marker marker = new Marker();
+		// create marking directory
+		File directory = new File(marker.dir);
+		
+
+		// attempt to cleanup previous students data
+		try {
+			marker.delete(directory);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//create new marking directory
+		directory.mkdir();
+		// processing arguments
+		marker.unzipMasterJar(args[0]);
+		marker.unzipJar(args[1], marker.dir);
+		// marker.generateList();
+		// diffing files
+		marker.diffFiles();
+		// marker.cleanup();
+		System.out.println("Detecting additional files...:");
+		marker.findExtras(directory);
+		System.out.println("Finished detecting additional files");
+
+		System.out.println("====================");
+		System.out.println("Parsing complete. \nNumber of files compared : " + marker.files.size());
+		System.out.println("Additional files : " + marker.addFiles);
+		System.out.println("Missing files : "+marker.missingFiles);
+
+	}
+
+}
